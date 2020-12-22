f10950c50d5394b6375e1a4778fb5f71b731dda4
alexanderhess <Hess.alexander1@web.de>
2020-12-22 13:49:32 +0100

    Fixes #2 Updated the README

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # easy-aliaser
 CLI-Toolkit to make editing your .zshrc file easier.
 
-Currently you can read out all the aliases in your .zshrc file on MacOs
+## About
+
+Currently this tool is able to
+- print out all the self defined aliases in your *.zshrc* file.
+- add a new alias to your .zshrc file
+
+## Get Started
+
+- Clone this repository 
+- go inside the easy-aliaser directory
+- to read out your aliases write `swift run easy-aliaser read-aliases`
+- to add a new alias write `swift run easy-aliaser new-alias <alias> <command>`
+
+
+This tool is on the foundation of https://swift.org/blog/argument-parser/
--- Sources/easy-aliaser/main.swift
+++ Sources/easy-aliaser/main.swift
@@ -10,17 +10,20 @@ import ArgumentParser
 struct Alias: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Make Editing Your .zshrc Much Easier",
-        subcommands: [readAliases.self])
+        subcommands: [
+            readAliases.self,
+            NewAlias.self
+        ])
 }
 extension Alias {
     struct readAliases: ParsableCommand {
         static let configuration = CommandConfiguration(
-            abstract: "Reads All The Aliases In Your .zshrc File")
+            abstract: "Reads All The Self Defined Aliases From Your .zshrc File")
         
         func run() {
 
             print("These are your Aliases");
-            readFile(path: "/Users/alexanderhess/.zshrc")
+            readFile(path: "/Users/alexanderhess/testAlias")
            }
             func readFile(path: String) -> Int {
                 errno = 0
@@ -29,15 +32,47 @@ extension Alias {
                     return 1
                 }
                 while let line = readLine() {
-                    if(line.starts(with: "# alias")){
-                        print(line.dropFirst(8));
+                    if(line.starts(with: "alias")){
+                        print(line.dropFirst(6));
                     }
                 }
                 return 0
             }
     }
-}
-Alias.main();
+    struct NewAlias : ParsableCommand {
+        @Argument(help: "String which is a shortcut for a command") var alias: String
+        @Argument(help: "The command itself which will be shortcuted ") var command: String
+        
+        static let configuration = CommandConfiguration(
+            abstract: "Add a new Alias parameters are <alias> <command>")
+        
+        func run() {
+            let fileName = URL(fileURLWithPath: "/Users/alexanderhess/testAlias");
+            // This represents the new alias which will be inserted inside the zsh file
+            // now I have to inside the alias at the end of the file
+                let myAlias = "\n" + "alias " + alias + "=\"" + command + "\"";
+                
+                    
+                    if let fileUpdater = try? FileHandle(forUpdating: fileName) {
+
+                        // Function which when called will cause all updates to start from end of the file
+                        fileUpdater.seekToEndOfFile()
+
+                        // Which lets the caller move editing to any position within the file by supplying an offset
+                        fileUpdater.write(myAlias.data(using: .utf8)!)
+
+                        // Once we convert our new content to data and write it, we close the file and that’s it!
+                        fileUpdater.closeFile()
+                    }
+                }
+            }
+        }
+    
+
+
+
+    Alias.main();
+
 
 
 
